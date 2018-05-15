b4d3679ec4f43ff82d1ca9a73040f92bcc28b08d
Steve Longoria <stevehl25@gmail.com>
2018-05-14 23:56:19 -0700

    Ran node populatedb to create documents in mongodb

--- models/genre.js
+++ models/genre.js
@@ -4,16 +4,16 @@ var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema(
 	{
-		name: {type: String, required: true, min: 3, max: 100}
+		name: {type: String, required: true, min: 3, max: 100},
 	}
 );
 
 // Virtual for Genre URL
-GenreScheme
+GenreSchema
 .virtual('url')
 .get(function() {
 	return '/catalog/genre/' + this._id;
 });
 
 //Export the model
-module.exports = mongoose.model('Genre', GenreScheme);
+module.exports = mongoose.model('Genre', GenreSchema);
