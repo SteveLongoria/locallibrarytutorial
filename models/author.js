8a51c89cde5c744348564ca3676474c1298a4f40
Steve Longoria <stevehl25@gmail.com>
2018-05-21 20:05:20 -0700

    Add virtual property to author model for formatting Author born and death dates

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment'); // For date handling.
 
 var Schema = mongoose.Schema;
 
@@ -25,12 +26,30 @@ AuthorSchema
   return '/catalog/author/' + this._id;
 });
 
-// Virtual for Formatted Dates
-
-BookInstanceSchema
-.virtual('due_back_formatted')
+AuthorSchema
+.virtual('lifespan')
 .get(function () {
-  return moment(this.due_back).format('MMMM Do, YYYY');
+  var lifetime_string='';
+  if (this.date_of_birth) {
+      lifetime_string=moment(this.date_of_birth).format('MMMM Do, YYYY');
+      }
+  lifetime_string+=' - ';
+  if (this.date_of_death) {
+      lifetime_string+=moment(this.date_of_death).format('MMMM Do, YYYY');
+      }
+  return lifetime_string
+});
+
+AuthorSchema
+.virtual('date_of_birth_yyyy_mm_dd')
+.get(function () {
+  return moment(this.date_of_birth).format('YYYY-MM-DD');
+});
+
+AuthorSchema
+.virtual('date_of_death_yyyy_mm_dd')
+.get(function () {
+  return moment(this.date_of_death).format('YYYY-MM-DD');
 });
 
 
