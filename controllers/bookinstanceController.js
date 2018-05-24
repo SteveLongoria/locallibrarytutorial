81868bf41509bec85a5b05a6df1f9f37480c0010
Steve Longoria <stevehl25@gmail.com>
2018-05-23 22:09:39 -0700

    Required the Book Model in bookinstanceController.js

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,5 +1,6 @@
 const { body,validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
+var Book = require('../models/book');
 var BookInstance = require('../models/bookinstance');
 
 // Display list of all BookInstances.
